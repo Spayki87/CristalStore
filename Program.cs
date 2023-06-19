4c24e764bd69834dc09844e7bfb351311929ee44
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-19 15:50:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crystals_store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int cristalCount;
+            int cristalPrice = 5;
+            Console.Write("Введите начальное количество монет:");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы можете купить кристалы по " + cristalPrice + " монет.");
+            Console.Write("Сколько кристалов вам нужно:");
+            cristalCount = Convert.ToInt32(Console.ReadLine());
+            money -= cristalCount * cristalPrice;
+            Console.WriteLine("У вас осталось " + money + " монет и " + cristalCount + " кристалов.");
+        }
+    }
+}
